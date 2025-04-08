46935b4f9e1873f96dd3d2fc6e7e356bece0a731
thiagoolimpio <olimpiodeoliveirathiago@gmail.com>
2025-04-08 13:04:53 -0300

    anotação

---
+++ f_strings.py
@@ -0,0 +1,10 @@
+nome = "thiago"
+sobrenome = "olimpio"
+idade =  17
+ano_nascimento = 2007
+maioridade = idade >= 18 == True
+altura_metros = 1.75
+# Definindo variáveis
+linha = f"{nome}, tem {altura_metros} de altura e tem {idade} anos de idade"
+
+print(linha)
