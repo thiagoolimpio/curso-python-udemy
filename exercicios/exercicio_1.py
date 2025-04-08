4a1b13dcbd3a815315cf3e80663f4732eab54fe6
thiagoolimpio <olimpiodeoliveirathiago@gmail.com>
2025-04-08 12:00:36 -0300

    exercicios completos

---
+++ exercicios/exercicio_1.py
@@ -0,0 +1,13 @@
+nome = "thiago"
+sobrenome = "olimpio"
+idade = 17
+ano_nascimento = 2007
+maioridade = idade >= 18 == True
+altura_metros = 1.75
+# Definindo variáveis
+print("nome:", nome)
+print("sobrenome", sobrenome)
+print("idade:", idade)
+print("ano de nascimento", ano_nascimento)
+print("maioridade:", maioridade)
+print("altura em metros:", altura_metros)
---
+++ exercicios/exercicio_2.py
@@ -0,0 +1,7 @@
+#claculadora imc :
+# IMC = peso / (altura * altura)
+print(input("Digite seu nome: "))
+peso = float(input("Digite seu peso: "))
+altura = float(input("Digite sua altura: "))
+imc = peso / (altura * altura)
+print("Seu IMC é:", imc)
