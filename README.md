360018dc0d6a45ccb3306e82450319fd7a5fec7f
thiagoolimpio <olimpiodeoliveirathiago@gmail.com>
2025-04-09 16:03:58 -0300

    exercicio 3

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 primeiro commit !
+o conteudo aqui apresentado, é postado com finalidade de documentar meu aprendizado. 
+conteudo em sua maior parte se deve ao curso que eu estou fazendo pela udemy!
+
+obrigado! <3
---
+++ exercicios/exercicio_3.py
@@ -0,0 +1,10 @@
+primeiro_numero = input('digite um numero: ')
+segundo_numero = input('digite outro numero: ')
+if primeiro_numero >= segundo_numero:
+    print(
+     f'{primeiro_numero} é maior que {segundo_numero}'
+    )
+else :
+    print(
+     f'{segundo_numero} é maior que {primeiro_numero}'
+)
